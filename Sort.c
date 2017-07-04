cdbf70cbf745d9bcbc5a78b5123437e9b13ab673
lyl20170705 <welcomeyou2010@126.com>
2017-07-04 17:05:05 +0800

    update sort.c
    add shell sort algorithm

--- Sort.c
+++ Sort.c
@@ -9,3 +9,20 @@ void InsertSort(int *p,int length)
       p[j+1]=tmp;
   }
 }
+
+void ShellSort(int *p,int length)
+{
+  int tmp,delta,i,j;
+  delta=length;
+  do
+  {
+    delta=delta/3+1;
+    for(i=delta;i<length;i++)
+    {
+      tmp=p[i];
+      for(j=i-delta;j>=0 && p[j]>p[j+delta];j-=delta)
+        p[j+delta]=p[j];
+      p[j+delta]=p[j];
+    }
+  }while(delta>1);
+}
